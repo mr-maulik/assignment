1f993c7d5207f43cb687102f8afde54776ce15e7
mr-maulik <121292897+mr-maulik@users.noreply.github.com>
2023-03-15 18:56:29 +0530

    Add files via upload

---
+++ module3/module3.1/calculater.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main()
+{
+    int a, b;
+
+    printf("enter the valua of a and b");
+    scanf("%d%d",&a,&b);
+
+    printf("\n addition no:%d",a+b);
+    printf("\n subcraption no:%d",a-b);
+	printf("\n multiplication no:%d",a*b);
+	printf("\n divison no:%d",a/b);
+	printf("\n moduld no:%d",a%b);
+
+
+}
