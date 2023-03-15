fcd177b5e701c76437ba19a65971f858365d7c3c
mr-maulik <121292897+mr-maulik@users.noreply.github.com>
2023-03-15 19:11:56 +0530

    Add files via upload

---
+++ module3/module3.1/module3.2/factorial.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() 
+{
+    int num, i;
+    unsigned long long fact = 1;
+
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    
+    if (num < 0) {
+        printf("Error: factorial of a negative number doesn't exist.");
+    }
+    else {
+        
+        for (i = 1; i <= num; ++i) {
+            fact *= i;
+        }
+        printf("Factorial of %d = %llu", num, fact);
+    }
+
+    return 0;
+}
+
