f4770b813c7d76f98a6689d9993b221381936c9d
mr-maulik <121292897+mr-maulik@users.noreply.github.com>
2023-03-30 11:58:39 +0530

    Add files via upload

---
+++ module-4/module-4.1/oop.cpp
@@ -0,0 +1,24 @@
+OOP stands for Object-Oriented Programming, which is a programming paradigm that is based on the concept of "objects" that can contain data and behavior. OOP provides a way of organizing code into reusable, modular, and easy-to-maintain components.
+
+Here are some of the key concepts in OOP using C++:
+
+Classes and Objects:
+ A class is a blueprint or template that describes the properties and behaviors of a particular type of object. An object is an instance of a class.
+
+1)Encapsulation:
+ Encapsulation is the process of hiding the internal details of an object and exposing only the necessary information to the outside world. It is achieved through access modifiers such as public, private, and protected.
+
+2)Inheritance: 
+Inheritance is the ability of a class to inherit properties and methods from another class. The class that is being inherited from is called the base class or parent class, and the class that inherits from it is called the derived class or child class.
+
+3)Polymorphism:
+ Polymorphism is the ability of an object to take on many forms. It allows a derived class to override the methods of its base class or to have multiple methods with the same name but different parameters.
+
+4)Abstraction: 
+Abstraction is the process of creating a simplified representation of a complex system. In OOP, abstraction is achieved through the use of abstract classes and interfaces.
+
+5)Constructors and Destructors:
+ Constructors are special methods that are used to initialize the object when it is created. Destructors are special methods that are called when the object is destroyed, and they are used to clean up any resources that the object has allocated.
+
+6)Access Modifiers:
+ Access modifiers are keywords that determine the level of access that other classes have to the properties and methods of a class. The three access modifiers in C++ are public, private, and protected.
